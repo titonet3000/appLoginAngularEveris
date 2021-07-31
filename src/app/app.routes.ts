47c0808fb47e5847f1685facada16acc9c98c0f9
Carlos Augusto Ñaño Velasquez <cnanovel@everis.com>
2021-07-30 21:21:01 -0500

    Add guard canActivate

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from "@angular/router";
 import { BienvenidoComponent } from "./bienvenido/bienvenido.component";
+import { AuthGuard } from "./commons/guards/auth.guard";
 import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./login/login.component";
 
 export const AppRoutes: Routes = [
 
     {
-        path: "", component:HomeComponent,
+        path: "", component:HomeComponent, canActivate: [AuthGuard],
         children: [
 
             { path: "", component: BienvenidoComponent }, 
