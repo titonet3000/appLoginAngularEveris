be2e91dc8659753ab711e769a9f6815f56141d4e
Carlos Augusto Ñaño Velasquez <cnanovel@everis.com>
2021-08-04 20:31:05 -0500

    Add counted Client

--- src/app/home/components/home-header/home-header.component.ts
+++ src/app/home/components/home-header/home-header.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { Customer } from 'src/app/customer/models/customer';
+import { CustomerService } from 'src/app/customer/services/customer.service';
 
 @Component({
   selector: 'app-home-header',
   templateUrl: './home-header.component.html',
   styleUrls: ['./home-header.component.scss']
 })
-export class HomeHeaderComponent implements OnInit {
+export class HomeHeaderComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  countCustomer:number;
+  customer$:Observable<Customer[]>;
+  customerSubscription:Subscription;
+
+  constructor(private customerService: CustomerService) { }
 
   ngOnInit(): void {
+
+    this.countCustomer = 0;
+
+    this.customer$ = this.customerService.getCustomer$();
+    this.customerSubscription = this.customer$.subscribe(list => {
+      this.countCustomer = list.length;
+
+    });
   }
+
+  ngOnDestroy(): void {
+    if(this.customerSubscription!=null && this.customerSubscription!=undefined)
+    this.customerSubscription.unsubscribe();
+  }
+
 
 }
