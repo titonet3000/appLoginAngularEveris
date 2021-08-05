178c2a1f6b0f7f03066d73a75871edfb696dd228
Carlos Augusto Ñaño Velasquez <cnanovel@everis.com>
2021-08-04 19:43:51 -0500

    Add Form Customer

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Customer } from './models/customer';
 
 @Component({
   selector: 'app-customer',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerComponent implements OnInit {
 
+  customer:Customer;
+  listCustomer:Customer[] = [];
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.customer = new Customer();
+    this.listCustomer = [];
+  }
+
+  
+  acceso():void{
+
+    const entity:Customer = new Customer();
+    entity.CODIGO = this.customer.CODIGO;
+    entity.FIRSTNAME = this.customer.FIRSTNAME;
+    entity.LASTNAME = this.customer.LASTNAME;
+
+    this.listCustomer.push(entity);
+    console.log("LISTA CLIENTES: " + JSON.stringify(this.listCustomer));
+    this.cleanCustomer();
+
+
+
+  }
+
+  cleanCustomer(){
+    this.customer.CODIGO = "";
+    this.customer.FIRSTNAME = "";
+    this.customer.LASTNAME = "";
   }
 
 }
